34e8b2c9225b3beb42bd9ad9730039a5fdefba64
d12o6aa <doaakarem798@gmail.com>
2024-02-16 19:21:05 +0200

    1th

---
+++ 001/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: integer number
+ * @b: integer number
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
