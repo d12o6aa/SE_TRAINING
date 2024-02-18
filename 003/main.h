5b7132e7d77b36af1798dd432911364f014bcd89
d12o6aa <doaakarem798@gmail.com>
2024-02-18 21:45:06 +0200

    update

---
+++ 003/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+void simple_print_buffer(char *buffer, unsigned int size);
+char *create_array(unsigned int size, char c);
+
+#endif
