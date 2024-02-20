ec1c82e078d81339c3285ae2b4f833a052c6bb35
d12o6aa <doaakarem798@gmail.com>
2024-02-20 21:25:10 +0200

    task 0

--- 005/0-sum_them_all.c
+++ 005/0-sum_them_all.c
@@ -1,2 +1,20 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list args;
+    unsigned int i, sum;
+
+    if (n == 0)
+        return (0);
+    
+    va_start(args, n);
+    sum = 0;
+    for (i = 0; i < n; i++)
+    {
+        sum += va_arg(args, unsigned int);
+    }
+
+    va_end(args);
+    return (sum);
+}
--- 005/variadic_functions.h
+++ 005/variadic_functions.h
@@ -1,2 +1,9 @@
-#include <stdio.h>
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /*VARIADIC_FUNCTION_H*/
